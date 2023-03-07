463a51b22d007e383baa089199c933cbc5bcb2bf
dougDimmadome420 <andre010400@web.de>
2023-03-07 16:14:10 +0100

    Adding features - More commands - Fixing connection issues after switching ports of minecraft server

--- minecraft/serverStatus.js
+++ minecraft/serverStatus.js
@@ -1,23 +1,26 @@
 import * as util from 'minecraft-server-util';
 
+const HOST = 'localhost';
+const PORT = 25564;
+
 export async function isActive() {
     try {
-        return await util.status('localhost');
+        return await util.status(HOST, PORT);
     } catch (err) {
         return false;
     }
 }
 
 export async function getWhich() {
-    const status = await util.status('localhost');
+    const status = await util.status(HOST, PORT);
     return `${status.motd.clean} (${status.version.name})`;
 }
 
 export async function playerCount() {
-    const status = await util.status('localhost');
+    const status = await util.status(HOST, PORT);
     return status.players.online;
 }
 
 export async function getStatus() {
-    return await util.status('localhost');
+    return await util.status(HOST, PORT);
 }
